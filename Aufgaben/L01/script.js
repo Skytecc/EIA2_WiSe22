e01e67fda89881a87e770bfa95a7ac1888087881
Skytecc <101884474+Skytecc@users.noreply.github.com>
2022-10-11 18:55:04 +0200

    Add new changes

--- Aufgaben/L01/script.js
+++ Aufgaben/L01/script.js
@@ -1,4 +1,39 @@
-let v = 1;
-v = v + 1;
-console.log(v);
+"use strict";
+let subjekte = [
+    "Octane",
+    "Lifeline",
+    "Seer",
+    "Wraith",
+    "Hades",
+    "Bloodhound"
+];
+let prädikate = [
+    "liebt",
+    "hasst",
+    "verrät",
+    "lernt",
+    "denkt",
+    "jagt"
+];
+let objekte = [
+    "die Gegner",
+    "seinen Sohn Zagreus",
+    "in New York",
+    "neue Strategien",
+    "Edward",
+    "die kalte Nacht"
+];
+function ausgabeArray(test) {
+    for (let i = 6; i < test.length; i--)
+        ;
+    return test;
+}
+console.log(subjekte);
+console.log(prädikate);
+console.log(objekte);
+/*function getVerse(arr01: string[], arr02: string[], arr03[]: string): string[] {
+
+    for (let i: number = )
+    return;
+}*/
 //# sourceMappingURL=script.js.map
--- Aufgaben/L01/script.ts
+++ Aufgaben/L01/script.ts
@@ -1,3 +1,46 @@
-let v: number = 1;
-v = v + 1;
-console.log(v);
+
+let subjekte: string[] = [
+    "Octane", 
+    "Lifeline", 
+    "Seer", 
+    "Wraith", 
+    "Hades", 
+    "Bloodhound"
+];
+
+let prädikate: string[] = [
+    "liebt",
+    "hasst",
+    "verrät",
+    "lernt",
+    "denkt",
+    "jagt"
+];
+
+let objekte: string[] =[
+    "die Gegner",
+    "seinen Sohn Zagreus",
+    "in New York",
+    "neue Strategien",
+    "Edward",
+    "die kalte Nacht"
+];
+
+function ausgabeArray(test: string[]): string[]{
+    for (let i: number = 6; i < test.length; i--);
+    return test;
+
+}
+
+console.log(subjekte);
+console.log(prädikate);
+console.log(objekte);
+
+/*function getVerse(arr01: string[], arr02: string[], arr03[]: string): string[] {
+
+    for (let i: number = )
+    return;
+}*/
+
+
+
