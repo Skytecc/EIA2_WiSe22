637efca8bc296605280418b80f9c2605e55bc464
Skytecc <101884474+Skytecc@users.noreply.github.com>
2022-10-13 12:32:13 +0200

    fix

--- Aufgaben/L01/script.js
+++ Aufgaben/L01/script.js
@@ -1,44 +1,51 @@
 "use strict";
-let subjekte = [
-    "Octane",
-    "Lifeline",
-    "Seer",
-    "Wraith",
-    "Hades",
-    "Bloodhound"
-];
-let prädikate = [
-    "liebt",
-    "hasst",
-    "verrät",
-    "lernt",
-    "denkt",
-    "jagt"
-];
-let objekte = [
-    "die Gegner",
-    "seinen Sohn Zagreus",
-    "in New York",
-    "neue Strategien",
-    "Edward",
-    "die kalte Nacht"
-];
+/*
+Aufgabe: <Nummer und Titel der Aufgabe>
+Name: <Cindy Nguyen>
+Matrikel: <271131>
+Datum: <13.10.2022>
+Quellen: <->
+*/
 var ZufallsGedicht;
 (function (ZufallsGedicht) {
+    let subjekte = [
+        "Octane",
+        "Lifeline",
+        "Seer",
+        "Wraith",
+        "Hades",
+        "Bloodhound"
+    ];
+    let prädikate = [
+        "liebt",
+        "hasst",
+        "verrät",
+        "lernt",
+        "denkt",
+        "jagt"
+    ];
+    let objekte = [
+        "die Gegner",
+        "seinen Sohn Zagreus",
+        "in New York",
+        "neue Strategien",
+        "Edward",
+        "die kalte Nacht"
+    ];
     for (let i = 6; i > 0; i--) {
         console.log(getVerse(subjekte, prädikate, objekte));
     }
+    function getVerse(_arr01, _arr02, _arr03) {
+        let randomNumber01 = Math.floor(Math.random() * _arr01.length);
+        let randomNumber02 = Math.floor(Math.random() * _arr02.length);
+        let randomNumber03 = Math.floor(Math.random() * _arr03.length);
+        let a = _arr01[randomNumber01];
+        let b = _arr02[randomNumber02];
+        let c = _arr03[randomNumber03];
+        // splice(startwert, Anzahl der Werte die spliced werden sollen)
+        _arr01.splice(randomNumber01, 1);
+        let vers = a + " " + b + " " + c;
+        return vers;
+    }
 })(ZufallsGedicht || (ZufallsGedicht = {}));
-function getVerse(_arr01, _arr02, _arr03) {
-    let randomNumber01 = Math.floor(Math.random() * _arr01.length);
-    let randomNumber02 = Math.floor(Math.random() * _arr02.length);
-    let randomNumber03 = Math.floor(Math.random() * _arr03.length);
-    let a = _arr01[randomNumber01];
-    let b = _arr02[randomNumber02];
-    let c = _arr03[randomNumber03];
-    // splice(startwert, Anzahl der Werte die spliced werden sollen)
-    _arr01.splice(randomNumber01, 1);
-    let vers = a + " " + b + " " + c;
-    return vers;
-}
 //# sourceMappingURL=script.js.map
--- Aufgaben/L01/script.ts
+++ Aufgaben/L01/script.ts
@@ -1,59 +1,68 @@
-let subjekte: string[] = [
-    "Octane", 
-    "Lifeline", 
-    "Seer", 
-    "Wraith", 
-    "Hades", 
-    "Bloodhound"
-];
-
-let prädikate: string[] = [
-    "liebt",
-    "hasst",
-    "verrät",
-    "lernt",
-    "denkt",
-    "jagt"
-];
-
-let objekte: string[] = [
-    "die Gegner",
-    "seinen Sohn Zagreus",
-    "in New York",
-    "neue Strategien",
-    "Edward",
-    "die kalte Nacht"
-];
-
+/*
+Aufgabe: <Nummer und Titel der Aufgabe>
+Name: <Cindy Nguyen>
+Matrikel: <271131>
+Datum: <13.10.2022>
+Quellen: <->
+*/
 namespace ZufallsGedicht {
 
-for (let i: number = 6; i > 0; i-- ) {
-       console.log(getVerse(subjekte, prädikate, objekte));
+    let subjekte: string[] = [
+        "Octane",
+        "Lifeline",
+        "Seer",
+        "Wraith",
+        "Hades",
+        "Bloodhound"
+    ];
+
+    let prädikate: string[] = [
+        "liebt",
+        "hasst",
+        "verrät",
+        "lernt",
+        "denkt",
+        "jagt"
+    ];
+
+    let objekte: string[] = [
+        "die Gegner",
+        "seinen Sohn Zagreus",
+        "in New York",
+        "neue Strategien",
+        "Edward",
+        "die kalte Nacht"
+    ];
+
+    for (let i: number = 6; i > 0; i--) {
+        console.log(getVerse(subjekte, prädikate, objekte));
+    }
+
+    function getVerse(_arr01: string[], _arr02: string[], _arr03: string[]): string {
+
+
+        let randomNumber01: number = Math.floor(Math.random() * _arr01.length);
+        let randomNumber02: number = Math.floor(Math.random() * _arr02.length);
+        let randomNumber03: number = Math.floor(Math.random() * _arr03.length);
+
+        let a: string = _arr01[randomNumber01];
+        let b: string = _arr02[randomNumber02];
+        let c: string = _arr03[randomNumber03];
+
+        // splice(startwert, Anzahl der Werte die spliced werden sollen)
+        _arr01.splice(randomNumber01, 1);
+
+
+        let vers: string = a + " " + b + " " + c;
+
+        return vers;
+
     }
 
 }
 
-function getVerse(_arr01: string[], _arr02: string[], _arr03: string[]): string {
-
-
-    let randomNumber01: number = Math.floor(Math.random() * _arr01.length);
-    let randomNumber02: number = Math.floor(Math.random() * _arr02.length);
-    let randomNumber03: number = Math.floor(Math.random() * _arr03.length);
-
-    let a: string = _arr01[randomNumber01];
-    let b: string = _arr02[randomNumber02];
-    let c: string = _arr03[randomNumber03];
-
-    // splice(startwert, Anzahl der Werte die spliced werden sollen)
-    _arr01.splice(randomNumber01, 1);
-
-
-    let vers: string = a + " " + b + " " + c;
-
-    return vers;
-
-    }
 
 
 
-    
+
+
