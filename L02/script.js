050f70217a3e315165a8de4f04552a27f3c0cb39
Skytecc <101884474+Skytecc@users.noreply.github.com>
2022-10-20 05:53:58 +0200

    trying to fix Position

--- L02/script.js
+++ L02/script.js
@@ -2,18 +2,45 @@
 var L02_Event_Inspector;
 (function (L02_Event_Inspector) {
     console.log("Start");
+    let infoString;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         let button = document.getElementById("button");
+        let div0 = document.getElementById("div0");
+        let div1 = document.getElementById("div1");
+        //let span1: HTMLElement = <HTMLElement>document.getElementById("span1");
         document.addEventListener("click", logInfo);
+        div0.addEventListener("click", logInfo);
+        div1.addEventListener("click", logInfo);
         document.addEventListener("keyup", logInfo);
-        //document.addEventListener("mousemove",setInfoBox);
+        div0.addEventListener("keyup", logInfo);
+        div1.addEventListener("keyup", logInfo);
+        document.addEventListener("mousemove", setInfoBox);
     }
     function logInfo(_event) {
         console.log(_event);
     }
     function setInfoBox(_event) {
-        console.log(_event);
+        //console.log(_event);
+        let x = _event.offsetX;
+        let y = _event.offsetY;
+        let xPosition = _event.clientX;
+        let yPostion = _event.clientY;
+        let coor = "(" + String(yPostion) + String(xPosition) + ")";
+        //console.log(xPosition + yPostion);
+        let info = _event.target;
+        let infoBox = document.createElement("span");
+        info.appendChild(infoBox);
+        infoString = coor;
+        infoBox.textContent = infoString;
+        infoBox.style.top = y + "px";
+        infoBox.style.left = x + "px";
+        info.addEventListener("mousemove", removePosition);
+    }
+    function removePosition(_event) {
+        let target = _event.target;
+        let parent = target.parentNode;
+        parent.removeChild(target);
     }
 })(L02_Event_Inspector || (L02_Event_Inspector = {}));
 //# sourceMappingURL=script.js.map
--- L02/script.ts
+++ L02/script.ts
@@ -1,14 +1,24 @@
 namespace L02_Event_Inspector {
     console.log("Start");
+    let infoString: string;
     window.addEventListener("load", handleLoad);
 
     function handleLoad(_event: Event): void {
 
         let button: HTMLElement = <HTMLElement>document.getElementById("button");
+        let div0: HTMLElement = <HTMLElement>document.getElementById("div0");
+        let div1: HTMLElement = <HTMLElement>document.getElementById("div1");
+        //let span1: HTMLElement = <HTMLElement>document.getElementById("span1");
 
         document.addEventListener("click",logInfo);
+        div0.addEventListener("click",logInfo);
+        div1.addEventListener("click",logInfo);
+
         document.addEventListener("keyup",logInfo);
-        //document.addEventListener("mousemove",setInfoBox);
+        div0.addEventListener("keyup",logInfo);
+        div1.addEventListener("keyup",logInfo);
+
+        document.addEventListener("mousemove",setInfoBox);
 
     }
 
@@ -17,8 +27,34 @@ namespace L02_Event_Inspector {
     }
 
     function setInfoBox(_event: MouseEvent): void {
-        console.log(_event);
+        //console.log(_event);
+        let x: number = _event.offsetX;
+        let y: number = _event.offsetY;
 
+        let xPosition: number = _event.clientX;
+        let yPostion: number = _event.clientY;
+        let coor: string = "(" + String(yPostion) + String(xPosition) + ")";
+
+        //console.log(xPosition + yPostion);
+        let info: HTMLElement = <HTMLElement>_event.target;
+        let infoBox: HTMLSpanElement = document.createElement("span");
+
+        info.appendChild(infoBox);
+
+        infoString = coor; 
+
+        infoBox.textContent = infoString;
+        infoBox.style.top =  y + "px";
+        infoBox.style.left =  x + "px";
+
+        info.addEventListener("mousemove", removePosition);
+
+    }
+
+    function removePosition(_event: MouseEvent): void {
+        let target: Node = <Node>_event.target;
+        let parent: Node = <Node>target.parentNode;
+        parent.removeChild(target);
     }
 
     
