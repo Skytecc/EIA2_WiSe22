0d328f81ee9fd2e4fdd0b001d4b62843f85f7002
Skytecc <101884474+Skytecc@users.noreply.github.com>
2022-10-20 04:11:07 +0200

    Grundstruktur ADD

--- L02/script.js
+++ L02/script.js
@@ -1,2 +1,18 @@
 "use strict";
+var L02_Event_Inspector;
+(function (L02_Event_Inspector) {
+    console.log("Start");
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        let button = document.getElementById("button");
+        document.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+    }
+    function logInfo(_event) {
+        console.log(_event);
+    }
+    function setInfoBox(_event) {
+        console.log(_event);
+    }
+})(L02_Event_Inspector || (L02_Event_Inspector = {}));
 //# sourceMappingURL=script.js.map
---
+++ L02/script.ts
@@ -0,0 +1,25 @@
+namespace L02_Event_Inspector {
+    console.log("Start");
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(_event: Event): void {
+
+        let button: HTMLElement = <HTMLElement>document.getElementById("button");
+
+        document.addEventListener("click",logInfo);
+        document.addEventListener("keyup",logInfo);
+
+    }
+
+    function logInfo(_event: Event): void {
+        console.log(_event);
+    }
+
+    function setInfoBox(_event: MouseEvent): void {
+        console.log(_event);
+
+    }
+
+    
+
+}
