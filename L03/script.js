9a8b297e7debc96620aed663200060c008f08d95
Skytecc <101884474+Skytecc@users.noreply.github.com>
2022-10-29 04:53:30 +0200

    Add

--- L03/script.js
+++ L03/script.js
@@ -1,4 +1,25 @@
 "use strict";
-let dateInput = document.getElementById("date");
-dateInput.value = new Date().toISOString().substring(0, 10);
+var L03_Einkaufsliste;
+(function (L03_Einkaufsliste) {
+    let dateInput = document.getElementById("date");
+    dateInput.value = new Date().toISOString().substring(0, 10);
+    window.addEventListener("load", handleload);
+    function handleload() {
+        let addBtn = document.getElementById("button");
+        let editBtn = document.querySelector(".edit");
+        let deleteBtn = document.querySelector(".delete");
+        addBtn.addEventListener("click", adding);
+        editBtn.addEventListener("click", editing);
+        deleteBtn.addEventListener("click", deleting);
+    }
+    function editing() {
+        console.log("test");
+    }
+    function adding() {
+        console.log("add");
+    }
+    function deleting() {
+        console.log("delete");
+    }
+})(L03_Einkaufsliste || (L03_Einkaufsliste = {}));
 //# sourceMappingURL=script.js.map
--- L03/script.ts
+++ L03/script.ts
@@ -6,12 +6,29 @@ namespace L03_Einkaufsliste {
 
     window.addEventListener("load", handleload);
 
-    function handleLoad(): void {
-        let addBtn: HTMLButtonElement = document.getElementById("button");
-        
+    function handleload(): void {
+        let addBtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+        let editBtn: HTMLElement = <HTMLElement>document.querySelector(".edit");
+        let deleteBtn: HTMLElement = <HTMLElement>document.querySelector(".delete");
+
+        addBtn.addEventListener("click", adding);
+        editBtn.addEventListener("click", editing);
+        deleteBtn.addEventListener("click", deleting);
+
+    }
+
+    function editing(): void {
+        console.log("test");
+    }
+
+    function adding(): void {
+        console.log("add");
+    }
+
+    function deleting(): void {
+        console.log("delete");
+    }
+
 }
-
-
-
 
 
