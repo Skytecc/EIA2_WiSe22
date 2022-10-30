edc27d3c68fb72199d6c472696fc94353f37fbae
Skytecc <101884474+Skytecc@users.noreply.github.com>
2022-10-30 18:44:55 +0100

    fix css

--- L03/script.ts
+++ L03/script.ts
@@ -8,7 +8,7 @@ Quellen: <->
 namespace L03_Einkaufsliste {
 
     let dateInput: HTMLInputElement = <HTMLInputElement>document.getElementById("date");
-
+    
     dateInput.value = new Date().toISOString().substring(0, 10);
 
     window.addEventListener("load", handleload);
