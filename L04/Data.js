40b6bac7a88a0cb56352aace2f3434a03662507d
Skytecc <101884474+Skytecc@users.noreply.github.com>
2022-11-03 22:53:28 +0100

    add

--- L04/Data.js
+++ L04/Data.js
@@ -1,2 +1,11 @@
 "use strict";
+var L04_Einkaufsliste;
+(function (L04_Einkaufsliste) {
+    let currentItems = {
+        list: [
+            { name: "Banane", amount: 4, date: "30.10.2022", comment: "Für den Milchshake" },
+            { name: "Apfel", amount: 8, date: "30.10.2022", comment: "Für den smoothie" }
+        ]
+    };
+})(L04_Einkaufsliste || (L04_Einkaufsliste = {}));
 //# sourceMappingURL=Data.js.map
--- L04/Data.ts
+++ L04/Data.ts
@@ -9,4 +9,12 @@ namespace L04_Einkaufsliste {
     interface currentList {
         [showList: string]: Item[];
     }
+
+    let currentItems: currentList  = {
+        
+        list: [
+            { name: "Banane", amount: 4, date: "30.10.2022", comment: "Für den Milchshake"},
+            { name: "Apfel", amount: 8, date: "30.10.2022", comment: "Für den smoothie"}
+        ]
+    };
 }
