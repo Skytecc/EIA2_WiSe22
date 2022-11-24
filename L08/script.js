a696309585c4bf15075759fbf5080927e051a49f
Skytecc <101884474+Skytecc@users.noreply.github.com>
2022-11-24 16:02:59 +0100

    add

--- L08/script.js
+++ L08/script.js
@@ -14,12 +14,37 @@ var L08_Canvas;
     canvas.height = window.innerHeight;
     window.addEventListener("load", handleLoad);
     function handleLoad() {
-        //testfill();
         testGradient();
+        test3();
+        testCircle();
     }
-    function testfill() {
-        crc2.fillStyle = "#74847e";
-        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    function testCircle() {
+        //crc2.fillStyle = "#74847e";
+        //crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+        let radius = 500;
+        let angle = Math.atan2(100, 300) + Math.PI / 2;
+        let gx = radius * Math.cos(angle);
+        let gy = radius * Math.sin(angle);
+        let cx = (0 + 300) / 2;
+        let cy = (100 + 200) / 2;
+        let sizeCircle = Math.floor(Math.random() * 150);
+        let xCircle = Math.floor(Math.random() * crc2.canvas.width);
+        let yCircle = Math.floor(Math.random() * crc2.canvas.height);
+        let bezierCpx1 = Math.floor(Math.random() * 100);
+        let bezierCpy1 = Math.floor(Math.random() * 150);
+        let bezierCpx2 = Math.floor(Math.random() * 200);
+        let bezierCpy2 = Math.floor(Math.random() * 200);
+        let bezierX = crc2.canvas.width;
+        let bezierY = crc2.canvas.width;
+        let gradientCircle = crc2.createLinearGradient(cx - gx, cy - gy, cx + gx, cy + gy);
+        crc2.beginPath();
+        crc2.arc(xCircle, yCircle, sizeCircle, 0, 1.5 * Math.PI);
+        crc2.bezierCurveTo(bezierCpx1, bezierCpy1, bezierCpx2, bezierCpy2, bezierX, bezierY);
+        gradientCircle.addColorStop;
+        crc2.fillStyle = "black";
+        crc2.closePath();
+        crc2.stroke();
+        crc2.fill();
     }
     function testGradient() {
         let radius = 500;
@@ -35,6 +60,10 @@ var L08_Canvas;
         myGradient.addColorStop(0.5, "#00bc97");
         crc2.fillStyle = myGradient;
         crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    }
+    function test3() {
+        for (let i = 0; i < 20; i++) {
+        }
     }
 })(L08_Canvas || (L08_Canvas = {}));
 //# sourceMappingURL=script.js.map
--- L08/script.ts
+++ L08/script.ts
@@ -15,13 +15,53 @@ namespace L08_Canvas {
     window.addEventListener("load", handleLoad);
 
     function handleLoad(): void {
-        //testfill();
         testGradient();
+        test3();
+        testCircle();
     }
 
-    function testfill(): void {
-        crc2.fillStyle = "#74847e";
-        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    interface Vector {
+        x: number;
+        y: number;
+    }
+
+    function testCircle(): void {
+        //crc2.fillStyle = "#74847e";
+        //crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+
+        let radius: number = 500;
+
+        let angle: number = Math.atan2(100, 300) + Math.PI / 2;
+        let gx: number = radius * Math.cos(angle);
+        let gy: number = radius * Math.sin(angle);
+        let cx: number = (0 + 300) / 2;
+        let cy: number = (100 + 200) / 2;
+
+        let sizeCircle: number = Math.floor(Math.random() * 150);
+        let xCircle: number = Math.floor(Math.random() * crc2.canvas.width);
+        let yCircle: number = Math.floor(Math.random() * crc2.canvas.height);
+
+        let bezierCpx1: number = Math.floor(Math.random() * 100);
+        let bezierCpy1: number = Math.floor(Math.random() * 150);
+        let bezierCpx2: number = Math.floor(Math.random() * 200);
+        let bezierCpy2: number = Math.floor(Math.random() * 200);
+        let bezierX: number = crc2.canvas.width;
+        let bezierY: number = crc2.canvas.width;
+
+
+
+        let gradientCircle: CanvasGradient = crc2.createLinearGradient(cx - gx, cy - gy, cx + gx, cy + gy);
+
+
+        crc2.beginPath();
+        crc2.arc(xCircle, yCircle, sizeCircle, 0, 1.5 * Math.PI);
+        crc2.bezierCurveTo(bezierCpx1, bezierCpy1, bezierCpx2, bezierCpy2, bezierX, bezierY);
+        gradientCircle.addColorStop
+        crc2.fillStyle = "black";
+        crc2.closePath();
+        crc2.stroke();
+        crc2.fill();
+        
     }
 
     function testGradient(): void {
@@ -43,6 +83,13 @@ namespace L08_Canvas {
         crc2.fillStyle = myGradient;
         crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
 
+    }
+
+    function test3(): void {
+
+        for (let i: number = 0; i < 20; i++) {
+
+        }
 
     }
 
